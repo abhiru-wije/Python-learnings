ae5d17f60547c411b7991a0c96592b762e9579b6
abhiru-wije <abdwije19972219@gmail.com>
2021-11-17 17:47:25 +0530

    first python learnings

---
+++ Python - Normad coders/first.py
@@ -0,0 +1,13 @@
+days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+print(days)
+print(type(days))
+print("Mon" in days)  # common operator
+print(days[3])
+print(len(days))
+print(days)
+days.append("Sat")  # mutable operation
+print(days)
+days.insert(4, "Sun")
+print(days)
+days.reverse()
+print(days)
