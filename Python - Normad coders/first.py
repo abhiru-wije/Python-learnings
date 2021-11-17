0eddee3d03716c9d0897a1a9560c44bc94f7ceca
abhiru-wije <abdwije19972219@gmail.com>
2021-11-17 19:09:09 +0530

    Tuples and Dictionaries

--- Python - Normad coders/first.py
+++ Python - Normad coders/first.py
@@ -11,3 +11,21 @@ days.insert(4, "Sun")
 print(days)
 days.reverse()
 print(days)
+
+# for a list it can use both common and mutable operation
+# but tuples can only operate common operation
+sport = ("a", "b", "C", "D", "E")  # tuple
+print(sport)
+print(type(sport))
+# dictionary
+Abhiru = {
+    "name": "Abhiru",
+    "Age": 23,
+    "SriLankan": True,
+    "Single": False,
+    "Profession": "Data analytics",
+    "fav_food": ["Submarine", "Chocolate"]
+}
+print(Abhiru)
+Abhiru["handsome"] = True
+print(Abhiru)
